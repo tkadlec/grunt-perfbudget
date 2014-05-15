f785b0f1a719979f36bcd06806a8f29697dddfc7
Tim Kadlec <tim@timkadlec.com>
2014-05-15 10:37:09 -0500

    Submits basic test and grabs test ID

--- tasks/wpt.js
+++ tasks/wpt.js
@@ -16,35 +16,21 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('wpt', 'Grunt task for WebPageTest', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      url: "http://timkadlec.com",
+      key: "***REMOVED***",
+      location: "Dulles_Nexus5"
     });
+    var done = this.async(),
+        WebPageTest = require('webpagetest'),
+        wpt = new WebPageTest('www.webpagetest.org', options.key),
+        err, data;
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+        grunt.log.writeln( ('Running test...').cyan );
+        wpt.runTest(options.url, {location: options.location}, function(err, data) {
+          if (data.statusCode === 200) {
+            grunt.log.writeln( ('Test ID ' + data.data.testId + ' obtained....').cyan );
+          }
+        });
   });
 
 };
