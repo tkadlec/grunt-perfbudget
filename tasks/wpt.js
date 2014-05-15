8d73d309ffff756fccdd4671f66a5dddda87b232
Tim Kadlec <tim@timkadlec.com>
2014-05-15 15:18:26 -0500

    cleanup and comments

--- tasks/wpt.js
+++ tasks/wpt.js
@@ -41,6 +41,8 @@ module.exports = function(grunt) {
         curStatus,
         myTimer;
 
+    // takes the data returned by wpt.getTestResults and compares 
+    // to our budget thresholds
     var processData = function(data) {
       var budget = options.budget,
           median = data.data.median.firstView,
@@ -50,6 +52,7 @@ module.exports = function(grunt) {
       for (var item in budget) {
         // make sure this is objects own property and not inherited
         if (budget.hasOwnProperty(item)) {
+          //make sure it exists
           if (budget[item] !== '' && median.hasOwnProperty(item)) {
             if (median[item] > budget[item]) {
               pass = false;
@@ -61,6 +64,7 @@ module.exports = function(grunt) {
         }
       }
 
+      //output our header and results
       if (!pass) {
         grunt.log.error('\n\n-----------------------------------------------' +
               '\nTest for ' + options.url + ' \t  FAILED' +
@@ -77,16 +81,19 @@ module.exports = function(grunt) {
 
     var retrieveResults = function() {
       var response;
+      // try to get the results for the test
       wpt.getTestResults(testId, function(err, data) {
         response = data.response;
         if (response.statusCode === 200) {
-          clearTimeout(myTimer);
+          //yay! Let's process it now
           processData(response); 
         } else {
           if (response.statusCode !== curStatus) {
+            //update status so folks now we haven't died on them
             grunt.log.writeln( (response.statusText + '...').cyan );
             curStatus = response.statusCode;
           }
+          //check again later
           myTimer = setTimeout(function(testId) {
             retrieveResults(testId);
           }, 1000);
@@ -95,22 +102,26 @@ module.exports = function(grunt) {
     };
     var done = this.async(),
         WebPageTest = require('webpagetest'),
-        spawn = require('child_process').spawn,
         wpt = new WebPageTest(options.wptInstance, options.key),
         err, data;
 
-
+        // run the test
         wpt.runTest(options.url, {location: options.location, video: 1}, function(err, data) {
           if (err) {
+            // ruh roh!
             grunt.log.error(err);
           } else if (data.statusCode === 200) {
+            // yay! Let's let them know the test is running.
             grunt.log.writeln( ('Running test...').cyan );
 
             testId = data.data.testId;
+            //and we have an ID!
             grunt.log.writeln( ('Test ID ' + testId + ' obtained....').cyan );
             
+            //now try to get the actual results
             retrieveResults(testId);
           } else {
+            // ruh roh! Something is off here.
             grunt.log.error(data.statusText);
           }
         });
