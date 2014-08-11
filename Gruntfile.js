2f90b741766dfb00025388e534a8e42b9fc59888
Tim Kadlec <tim@timkadlec.com>
2014-08-11 14:27:49 -0500

    Temporarily removing nodeunit from Grunt tasks until I stop being a bad citizen

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'perfbudget', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'perfbudget']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
