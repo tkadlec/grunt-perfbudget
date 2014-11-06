71a206beb9365ffccdfcb0e7dfb98228fd56ea81
Tim Kadlec <tim@timkadlec.com>
2014-11-06 20:17:25 +0100

    minor tweak

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
       foo: {
         options: {
           url: 'http://google.com',
-          key: '81c119174fe742f7a2d778d0a505d096',
+          key: 'YOUR_API_KEY_HERE',
           budget: {
             SpeedIndex: '1500'
           }
